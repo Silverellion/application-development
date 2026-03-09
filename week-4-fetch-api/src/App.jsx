ce1da52df26c126b43e692040099d61907a2b68b
H82M19\Student <Student>
2026-03-09 13:43:32 +0700

    Add loading & error state

--- week-4-fetch-api/src/App.jsx
+++ week-4-fetch-api/src/App.jsx
@@ -4,20 +4,46 @@ import "./App.css"
 const App = () => {
 
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  // useEffect(() => {
+  //   const url = "https://jsonplaceholder.typicode.com/users"
+  //   fetch(url).then((response) => {
+  //     return response.json()
+  //   }).then((data) => {
+  //     setUsers(data)
+  //     console.log(data)
+  //   })
+  // }, [])
 
   useEffect(() => {
-    const url = "https://jsonplaceholder.typicode.com/users"
-    fetch(url).then((response) => {
-      return response.json()
-    }).then((data) => {
-      setUsers(data)
-      console.log(data)
-    })
+    async function fetchData() {
+      setTimeout(() => {
+        setLoading(false)
+          const url = "https://jsonplaceholder.typicode.com/users"
+          fetch(url).then((response) => {
+            if(!response.ok)
+              setError(true);
+            return response.json()
+          }).then((data) => {
+            if(!data || data.length === 0) {
+              setError(true)
+            } else {
+              setUsers(data)
+            }
+            console.log(data)
+          })
+      }, 1000)
+    }
+    fetchData()
   }, [])
-
 
 return (
   <>
+    {loading?"Loading....":"Finished Loading!"}
+    <br />
+    {error?"Error!":""}
     {users.map((user) => (
       <>
         <div key={user.id}>
