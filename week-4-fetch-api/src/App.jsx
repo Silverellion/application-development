66144ca4f1bf4bbd014a2a7c45daa2c0076eff25
H82M19\Student <Student>
2026-03-09 14:46:18 +0700

    Add filtering to fetched jsons

--- week-4-fetch-api/src/App.jsx
+++ week-4-fetch-api/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import "./App.css"
 
 const App = () => {
+
   // BÀI 1 & 2
   // const [users, setUsers] = useState([]);
   // const [loading, setLoading] = useState(true)
@@ -45,75 +46,114 @@ const App = () => {
   //     ))}
   //   </>
   // );
+
   // BÀI 3:
+  // const [tempUserId, setTempUserId] = useState("");
+  // const [url, setUrl] = useState("")
+  // const [user, setUser] = useState("")
+  // const [error, setError] = useState(false)
 
-  const [tempUserId, setTempUserId] = useState("");
-  const [url, setUrl] = useState("")
-  const [user, setUser] = useState("")
-  const [error, setError] = useState(false)
+  // const urlTemplate = "https://jsonplaceholder.typicode.com/users/"
 
-  const urlTemplate = "https://jsonplaceholder.typicode.com/users/"
+  // function searchButton() {
+  //   setUrl("")
+  //   setUser("")
+  //   setError(false)
 
-  function searchButton() {
-    setUrl("")
-    setUser("")
-    setError(false)
+  //   if(tempUserId === "")
+  //     return;
 
-    if(tempUserId === "")
-      return;
+  //   const newUrl = urlTemplate + tempUserId;
+  //   setUrl(newUrl)
+  //   console.log(url)
 
-    const newUrl = urlTemplate + tempUserId;
-    setUrl(newUrl)
-    console.log(url)
+  //   setTempUserId("")
 
-    setTempUserId("")
+  //   fetch(newUrl)
+  //     .then((response) => { 
+  //       if(!response.ok) {
+  //         setError(true)
+  //         return;
+  //       }
+  //       return response.json()
+  //     })
+  //     .then((data) => {
+  //       if(!data || data.length === 0) {
+  //         setError(true)
+  //         return;
+  //       }
 
-    fetch(newUrl)
-      .then((response) => { 
-        if(!response.ok) {
-          setError(true)
-          return;
-        }
-        return response.json()
+  //       setError(false)
+  //       setUser(data);
+  //       console.log(data)
+  //     })
+  // }
+
+  // return (
+  //   <>
+  //     <div>
+  //       <input
+  //         type="text" 
+  //         value={tempUserId}
+  //         onChange={((e) => (setTempUserId(e.target.value)))}
+  //         />
+  //     </div>
+  //     <button onClick={searchButton} className='search-button'>
+  //       Search
+  //     </button>
+
+  //     {!error && (
+  //       <div>
+  //         <p>{user.name}</p>
+  //         <p>{user.phone}</p>
+  //         <p>{user.email}</p>
+  //       </div>
+  //     )}
+
+  //     {error && (
+  //       <div>
+  //         Error loading data!
+  //       </div>
+  //     )}
+  //   </>
+  // )
+
+  // BÀI 4:
+
+  const [searchText, setSearchText] = useState("")
+  const [fetchedData, setFetchedData] = useState([])
+  const [displayPosts, setDisplayPosts] = useState([])
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
       })
       .then((data) => {
-        if(!data || data.length === 0) {
-          setError(true)
-          return;
-        }
-
-        setError(false)
-        setUser(data);
+        setFetchedData(data)
         console.log(data)
       })
+  }, [])
+
+  function searchButton() {
+    const filteredData = fetchedData.filter((temp) => temp.toLowerCase().includes(searchText.toLowerCase()))
+    console.log(filteredData)
   }
 
   return (
     <>
       <div>
-        <input
-          type="text" 
-          value={tempUserId}
-          onChange={((e) => (setTempUserId(e.target.value)))}
-          />
+        <input 
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}  
+        />
+        <button onClick={searchButton} className='search-button'>
+          Search
+        </button>
       </div>
-      <button onClick={searchButton} className='search-button'>
-        Search
-      </button>
-
-      {!error && (
-        <div>
-          <p>{user.name}</p>
-          <p>{user.phone}</p>
-          <p>{user.email}</p>
-        </div>
-      )}
-
-      {error && (
-        <div>
-          Error loading data!
-        </div>
-      )}
     </>
   )
 }
