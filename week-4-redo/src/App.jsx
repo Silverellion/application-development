1949c273b9f41fc2bdc516ace7850c9f7b62b699
H82M19\Student <Student>
2026-03-16 14:35:24 +0700

    add Json server example

--- week-4-redo/src/App.jsx
+++ week-4-redo/src/App.jsx
@@ -2,31 +2,59 @@ import React, { useEffect, useState } from 'react'
 import "./App.css"
 
 const App = () => {
-    const [users, setUsers] = useState([])
-    const url = "https://jsonplaceholder.typicode.com/users"
-
-
+    const [clients, setClients] = useState([]);
+    const [displayClients, setDisplayClients] = useState([]);
+    const [searchClientText, setSearchClientText] = useState("");
+    const url = "http://localhost:3000/clients"
 
     useEffect(() => {
-        fetch(url).then((response) => {
+        if(clients.length === 0 || !clients) {
+                    fetch(url).then((response) => {
             return response.json()
-        }).then((data) => {
-            console.log(data)
-            setUsers(data)
-        })
+            }).then((data) => {
+                console.log("Fetched Clients successfully!")
+                setClients(data)
+            })
+        }
     }, [])
 
-  return (
-    <>
-        {users.map((user) => (
-            <div key={user.id}>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-                <br />
-            </div>
-        ))}
-    </>
-  )
+    function buttonSearch() {
+        setDisplayClients([])
+        var tempSearchClient = searchClientText;
+        setDisplayClients("");
+        const filteredClient = clients.filter(c =>
+            JSON.stringify(c).toLowerCase().includes(tempSearchClient.toLocaleLowerCase())
+        )
+        setDisplayClients(filteredClient);
+        console.log(displayClients);
+    }
+
+    return(
+        <>
+            Look up client:
+            <input type="text" 
+                placeholder="Enter client's details here!"
+                value = {searchClientText}
+                onChange={(e) => setSearchClientText(e.target.value)}
+            /> 
+
+            <button onClick={buttonSearch}>
+                Search
+            </button>
+
+            {clients.map((client, index) => (
+                <div key={client.id}>
+                    <h1>Client #{index + 1}'s</h1>
+                    <p>Client's name: {client.name}</p>
+                    <p>Client's gender: {client.gender}</p>
+                    <p>Client's age: {client.age}</p>
+                </div>
+            ))}
+        </>
+    )
+
 }
+
+
 
 export default App
