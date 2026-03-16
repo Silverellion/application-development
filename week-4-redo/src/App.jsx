1717edee394079c4423b59cb86481fe0b3c4a6a8
H82M19\Student <Student>
2026-03-16 14:52:02 +0700

    Remove the reference JSON object

--- week-4-redo/src/App.jsx
+++ week-4-redo/src/App.jsx
@@ -117,15 +117,5 @@ const App = () => {
         </>
     )
 }
-    // "id": "59761c233d8d0f92a6b0570d",
-    // "isActive": true,
-    // "age": 24,
-    // "name": "Kirsten Sellers",
-    // "gender": "female",
-    // "company": "EMERGENT",
-    // "email": "kirstensellers@emergent.com",
-    // "phone": "+1 (831) 564-2190",
-    // "address": "886 Gallatin Place, Fannett, Arkansas, 4656"
-
 
 export default App
