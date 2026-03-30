24b3a8a01b47faf36564d426942dcbe247155a7a
H82M19\Student <Student>
2026-03-30 14:20:10 +0700

    Implement useNavigate

--- week-7/src/components/Products.jsx
+++ week-7/src/components/Products.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
 const Products = () => {
     const {id} = useParams();
+    const navigate = useNavigate();
   return (
     <>
-        <div>
-            <h3>Product ID: {id}</h3>
-        </div>
-        <div>
-            <p>Back to home: <Link to = "/">Home</Link></p>
-        </div>
+      <div>
+          <h3>Product ID: {id}</h3>
+      </div>
+      <div>
+        <button onClick={() => navigate("/")}>Purchase</button>
+      </div>
+      <div>
+          <p>Back to home: <Link to = "/">Home</Link></p>
+      </div>
     </>
     
   )
