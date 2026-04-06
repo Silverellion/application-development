a689d76f770ccab99dece1b1e01d48791c71ea42
H82M19\Student <Student>
2026-04-06 14:49:13 +0700

    Implement Dockerfile

--- week-8/src/App.jsx
+++ week-8/src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { use } from 'react'
 import "./App.css"
 import CountDisplayer from './components/CountDisplayer'
 import CountUpdater from './components/CountUpdater'
-import { RecoilRoot } from 'recoil'
+import { RecoilRoot, useRecoilState } from 'recoil'
+import themeAtom from './atoms/themeAtom'
 
 const App = () => {
+  const [theme, setTheme] = useRecoilState(themeAtom);
+  function updateTheme() {
+    if(theme === 0)
+      setTheme(1)
+    else
+      setTheme(0)
+  }
+
   return (
     <>
       <CountDisplayer/>
-      <CountUpdater/> 
+      <CountUpdater/>
+      <hr />
+      <button onClick={updateTheme}>Switch theme</button>
+      <p>Current theme: {theme}</p> 
     </>
   )
 }
